551ae72732cd6db3867b024ec08051dd94be4da7
Ben Breier <ben.breier@airbnb.com>
2020-12-13 14:38:05 -0800

    First commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/Dropbox/org/ben-work-stuff.org" "/Users/ben_breier/Dropbox/org/journal/23112020.org" "/Users/ben_breier/Dropbox/org/journal/07122020.org" "/Users/ben_breier/Dropbox/org/journal/04122020.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
